f3a5eae3b948de20c09662837794e3be2f98fc61
pm-patel12 <pmp372418@gmail.com>
2025-11-19 21:11:58 +0530

    feat: fix the import issue

--- routes/store.js
+++ routes/store.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const StoreController = require("../controllers/StoreController");
+const StoreController = require("../controllers/storeController");
 const authMiddleware = require("../authentication");
 
 router.get("/allStores", authMiddleware, StoreController.getAllStores);
